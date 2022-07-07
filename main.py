1a4910bb764f17a555db05a9a1554ab4869d02a0
nzkea <bookwormnathan@gmail.com>
2022-07-07 12:09:33 +1200

    CLI config for wallets and contracts.

--- main.py
+++ main.py
@@ -5,13 +5,13 @@ import dotenv
 import typer
 import json
 import os
+from web3 import Web3
 
 from prompt_toolkit.shortcuts import yes_no_dialog
-from prompt_toolkit.shortcuts import message_dialog
 from prompt_toolkit.shortcuts import input_dialog
 from prompt_toolkit.shortcuts import button_dialog
-from prompt_toolkit.validation import Validator, ValidationError
-from prompt_toolkit import prompt
+from prompt_toolkit.shortcuts import message_dialog
+
 from dotenv import load_dotenv
 
 from modules import contract_interactions
@@ -55,13 +55,16 @@ def confirm_dialog(title, message):
         text=message).run()
 
 
-def text_in_dialog(title, message):
-    return input_dialog(
-        title=title,
-        text=message).run()
+def text_in_dialog(titles_messages):
+    inputs = []
+    for dialog in titles_messages:
+        inputs.append(input_dialog(
+            title=dialog[0],
+            text=dialog[1]
+            ).run())
+    return inputs
 
-
-def wallet_choice_dialog(title, message):
+def choice_dialog(title, message):
     return button_dialog(
         title=title,
         text=message,
@@ -73,37 +76,80 @@ def wallet_choice_dialog(title, message):
     ).run()
 
 
-def wallet_address_builder(addresses, new_wallet):
-    address_string = '\'['
+def address_builder(addresses, new):
+    address_string = '['
     for address in addresses:
         address_string = address_string + '"' + address + '"' + ","
-        print(address_string)
-    address_string += '"' + str(new_wallet) + '"' + "]'"
-    print(address_string)
+    address_string += '"' + str(new) + '"' + ']'
     return address_string
+
+
+def set_env(new, name):
+    os.environ[str(name)] = new
+    dotenv.set_key(".env", name, os.environ[str(name)])
 
 
 
 @app.command()
 def wallets():
     load_dotenv('.env')
+    message_dialog(
+        title='Warning',
+        text='Info - there is no input validation on wallet addresses, this means you need to get them right').run()
     while True:
-        wallet_addresses = (json.loads(os.getenv('test')))
-        wallet_choice = wallet_choice_dialog("Wallets",'Your current wallets: ' + str(wallet_addresses))
-        if wallet_choice:
-            new_wallet = text_in_dialog("Address", "Address:")
-            os.environ["test"] = wallet_address_builder(wallet_addresses, new_wallet)
-            dotenv.set_key(".env", "test", os.environ["test"])
-            text_in_dialog("Private Key", "Key:")
+        wallet_addresses = (json.loads(os.getenv('wallet_addresses')))
+        keys = (json.loads(os.getenv('wallet_addresses')))
+        wallet_choice = choice_dialog("Wallets",'Your current wallets: ' + str(wallet_addresses))
+        if wallet_choice: 
+            wallet_dialogs = text_in_dialog([["Address", "Address:"], ["Private Key", "Key:"]])
+            new_wallet = wallet_dialogs[0]
+            new_key = wallet_dialogs[1]
+            if new_wallet and new_key:
+                set_env(address_builder(wallet_addresses, new_wallet), "wallet_addresses")
+                set_env(address_builder(keys, new_key), "keys")
         elif wallet_choice == False:
-            print("reset")
+            set_env('[]', "wallet_addresses")
         else:
             break
+
+class new_contract():
+    def __init__(self, address, name, price, max, mint_function = "publicSaleMint"):
+        self.address = str(address)
+        self.name = str(name)
+        self.price = str(price)
+        self.mint_function = str(mint_function)
+        self.max = str(max)
+        self.json = self.create_json()
+    def create_json(self):
+        return {
+            self.address: {
+                "name": self.name,
+                "price": self.price,
+                "mint_function": self.mint_function,
+                "max": self.max
+            }
+        }
 
 
 @app.command()
 def contracts():
-    test = input("Hello")
+    while True:
+        with open("files/contracts.json","r+") as json_contract_list:
+            contracts_list = json.load(json_contract_list)
+            contract_choice = choice_dialog("Contracts", "Loaded contracts: " + str(contracts_list.keys()) )
+            if contract_choice:
+                new_contract_input = text_in_dialog([["Address", "Contract Address"],["Name", "Name (use anything you want)"],["Price","Price (0 for free)"],["Max","Max amount (limit per wallet)"],["Mint Func", "Contract Function (leave blank for Joepegs default)"]])
+                print(new_contract_input)
+                x = new_contract( new_contract_input[0], new_contract_input[1], new_contract_input[2],  new_contract_input[3])
+                contracts_list.update(x.json)
+            elif contract_choice == False:
+                contracts_list.clear()
+                json_contract_list.truncate(0)
+            else:
+                break
+            json_contract_list.seek(0)
+            json.dump(contracts_list, json_contract_list)
+
 
 
 
